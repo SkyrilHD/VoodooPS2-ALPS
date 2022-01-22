SkyrilHD/VoodooPS2-ALPS

commit 19f00342d814004d3783f397d6344ad21ab2c384

exclude VoodooInput loading on V1, V2 and V6 touchpads

VoodooPS2Trackpad/alps.cpp
@@ -3175,7 +3175,8 @@ void ALPS::set_protocol() {
             break;
     }
     
-    set_resolution();
+    if (priv.proto_version != (ALPS_PROTO_V1 | ALPS_PROTO_V2 | ALPS_PROTO_V6))
+        set_resolution();
 }
 
 bool ALPS::matchTable(ALPSStatus_t *e7, ALPSStatus_t *ec) {

commit 2edba228497abd620bf7bb8713d94843d4274d12

fix bug VoodooInput not working introduced in https://github.com/SkyrilHD/VoodooPS2-ALPS/commit/19f00342d814004d3783f397d6344ad21ab2c384

VoodooPS2Trackpad/alps.cpp
@@ -3175,7 +3175,9 @@ void ALPS::set_protocol() {
             break;
     }
     
-    if (priv.proto_version != (ALPS_PROTO_V1 | ALPS_PROTO_V2 | ALPS_PROTO_V6))
+    if (priv.proto_version != ALPS_PROTO_V1 ||
+        priv.proto_version != ALPS_PROTO_V2 ||
+        priv.proto_version != ALPS_PROTO_V6)
         set_resolution();
 }
 